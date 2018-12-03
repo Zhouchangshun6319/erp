932bd3da338375ea89f4d9eb7ff6cf43891b9022
sawyer.wu <wusy@lmfun.cn>
2018-12-03 10:12:25 +0800

    return demo

--- src/main/java/com/ace/demo/DemoApplication.java
+++ src/main/java/com/ace/demo/DemoApplication.java
@@ -2,11 +2,20 @@ package com.ace.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/demo")
+	public String demo() {
+		return "demo";
 	}
 }
